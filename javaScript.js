23389406d23bab6d3c9ce9c457c8d43de449cb7f
alphine3900 <alphine.kirimi@student.moringaschool.com>
2021-11-14 23:41:26 +0300

    add month,date and year formula

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,7 @@
 var form = document.getElementById("Akan_form");
 
 form.addEventListener("submit", function() {
+            let century = document.getElementById("date_year")
+            let bornMonth = document.getElementById("date_month")
+            let bornDate = document.getElementById("date_of_birth")
+            var CC = parseInt(century.value.slice(0, 2));
