63a9491808ffa3b1d8032069750905f4b6e999ee
alphine3900 <alphine.kirimi@student.moringaschool.com>
2021-11-14 23:53:46 +0300

    add the full functionality

--- javaScript.js
+++ javaScript.js
@@ -1,32 +1,37 @@
 var form = document.getElementById("Akan_form");
 
 form.addEventListener("submit", function() {
-            let century = document.getElementById("date_year")
-            let bornMonth = document.getElementById("date_month")
-            let bornDate = document.getElementById("date_of_birth")
-            var CC = parseInt(century.value.slice(0, 2));
-            var YY = parseInt(century.value.slice(2, 4));
-            var MM = parseInt(bornMonth.value);
-            var DD = parseInt(bornDate.value);
-            const Formula = ((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7).toFixed();
-            let index = "";
-            if (Formula == 0) {
-                index = 6;
+    let century = document.getElementById("date_year")
+    let bornMonth = document.getElementById("date_month")
+    let bornDate = document.getElementById("date_of_birth")
+    var CC = parseInt(century.value.slice(0, 2));
+    var YY = parseInt(century.value.slice(2, 4));
+    var MM = parseInt(bornMonth.value);
+    var DD = parseInt(bornDate.value);
+    const Formula = ((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7).toFixed();
+    let index = "";
+    if (Formula == 0) {
+        index = 6;
 
-            } else {
-                index = Formula - 1;
+    } else {
+        index = Formula - 1;
 
-            }
+    }
 
 
-            let total_male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-            let total_female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
-            let total_Weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    let total_male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+    let total_female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+    let total_Weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-            let maleval = document.getElementById("male")
-            let femaleval = document.getElementById("female")
+    let maleval = document.getElementById("male")
+    let femaleval = document.getElementById("female")
 
-            if (maleval.checked) {
-                var heads = document.getElementById("header_one");
-                heads.innerHTML = `Your Akan Name is ${total_male[index]} and born in ${total_Weeks[index]}`;
-            } else if (femaleval.checked) {
+    if (maleval.checked) {
+        var heads = document.getElementById("header_one");
+        heads.innerHTML = `Your Akan Name is ${total_male[index]} and born in ${total_Weeks[index]}`;
+    } else if (femaleval.checked) {
+        var heads = document.getElementById("header_one");
+        heads.innerHTML = `Your Akan Name is ${total_female[index]} and born in ${total_Weeks[index]}`;
+    }
+
+})
