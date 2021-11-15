2194ac0d6d54b6d795c1374db8aa6ab42cfe2a1f
alphine3900 <alphine.kirimi@student.moringaschool.com>
2021-11-15 19:11:56 +0300

    corrected error in js

--- javaScript.js
+++ javaScript.js
@@ -1,37 +1,30 @@
 var form = document.getElementById("Akan_form");
 
-form.addEventListener("submit", function() {
-    let century = document.getElementById("date_year")
-    let bornMonth = document.getElementById("date_month")
-    let bornDate = document.getElementById("date_of_birth")
-    var CC = parseInt(century.value.slice(0, 2));
-    var YY = parseInt(century.value.slice(2, 4));
-    var MM = parseInt(bornMonth.value);
-    var DD = parseInt(bornDate.value);
-    const Formula = ((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7).toFixed();
-    let index = "";
-    if (Formula == 0) {
-        index = 6;
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
 
-    } else {
-        index = Formula - 1;
+    var bornDate = document.getElementById("date_of_birth");
 
-    }
+    var mydate = new Date(bornDate.value)
 
 
-    let total_male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-    let total_female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
-    let total_Weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    var total_male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+    var total_female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+    var total_Weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-    let maleval = document.getElementById("male")
-    let femaleval = document.getElementById("female")
+    var maleval = document.getElementById("male")
+    var femaleval = document.getElementById("female")
 
     if (maleval.checked) {
         var heads = document.getElementById("header_one");
-        heads.innerHTML = `Your Akan Name is ${total_male[index]} and born in ${total_Weeks[index]}`;
+        heads.innerHTML = `Your Akan Name is ${total_male[mydate.getDay()]} and born in ${total_Weeks[mydate.getDay()]}`;
+        return false;
+
     } else if (femaleval.checked) {
         var heads = document.getElementById("header_one");
-        heads.innerHTML = `Your Akan Name is ${total_female[index]} and born in ${total_Weeks[index]}`;
+        heads.innerHTML = `Your Akan Name is ${total_female[mydate.getDay()]} and born in ${total_Weeks[mydate.getDay()]}`;
+        return false;
+
     }
 
 })
